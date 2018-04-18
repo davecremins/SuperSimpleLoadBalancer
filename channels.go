acc5709eedb0083737aa803aa2a896e5b56bf626
Dave Cremins <dave.cremins@wpengine.com>
2018-04-18 13:33:54 +0100

    Use tabs and custom log function

--- channels.go
+++ channels.go
@@ -1,50 +1,60 @@
 package main
 
 import (
-   "fmt"
-   "time"
+	"fmt"
+	"time"
 )
 
+const LOG_WORKER_ID bool = false
+
 type Work struct {
-   x, y, z int
+	x, y, z int
+}
+
+func log(msg string, id int) {
+	if LOG_WORKER_ID {
+		fmt.Println(msg, id)
+	}
 }
 
 // Use uni-directional channels
-func worker(in <-chan *Work, out chan<- *Work) {
-   for w := range in {
-      w.z = w.x * w.y
-      out <- w
-   }
-   close(out)
+func worker(in <-chan *Work, out chan<- *Work, id int) {
+	log("creating worker with id: ", id)
+	for w := range in {
+		log("processing with worker id: ", id)
+		w.z = w.x * w.y
+		out <- w
+	}
+	close(out)
 }
 
 // Specify a send only channel
-func sendLotsOfWork(in chan<- *Work){
-   for i := 0; i < 10; i++ {
-      time.Sleep(200 * time.Millisecond)
-      var work = &Work{i, i, i}
-      in <- work
-   }
-   close(in)
+func sendLotsOfWork(in chan<- *Work) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(200 * time.Millisecond)
+		var work = &Work{i, i, i}
+		in <- work
+	}
+	// time.Sleep(2000 * time.Millisecond)
+	close(in)
 }
 
-func receiveResult(out *Work){
-   fmt.Println(out.z)
+func receiveResult(r *Work) {
+	fmt.Println("Result from worker: ", r.z)
 }
 
 func Run() {
-   in, out := make(chan *Work), make(chan *Work)
-   go worker(in, out)
-   go sendLotsOfWork(in)
-   for {
-      result, ok := <-out
-      if !ok {
-         break
-      }
-      receiveResult(result)
-   }
+	in, out := make(chan *Work), make(chan *Work)
+	NumWorkers := 3
+	for i := 0; i < NumWorkers; i++ {
+		go worker(in, out, i)
+	}
+	go sendLotsOfWork(in)
+	for r := range out {
+		receiveResult(r)
+	}
 }
 
 func main() {
-   Run()
+	Run()
 }
